d5280403e3833d312612533f893445aa93fee99e
Vickie Garrett <vgarrett68@gmail.com>
2025-04-14 18:44:15 -0400

    Live deploy-ready ReloPro project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Welcome to ReloPro™</h1>
+      <p>Your real estate command center is ready to go!</p>
+    </div>
+  );
+}
